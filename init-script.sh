d8bbf78a1383ad7011d8f7aa425a1657d9656db3
Andre Arao <aarao@redhat.com>
2025-02-27 14:38:06 -0300

    Postgres no openshift

---
+++ init-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# script customizado para copiar o pg_hba.conf
+
+rm -rf /var/lib/postgresql/data/pgdata/postmaster.pid
+
+chown -R postgres:postgres /var/lib/postgresql/data/pgdata
+
+# Copia o arquivo pg_hba.conf do ConfigMap ou de outro local para o diretório de dados
+cp /etc/postgresql/pg_hba.conf /var/lib/postgresql/data/pgdata/pg_hba.conf
+
+# Executa o comando de inicialização padrão do PostgreSQL
+exec docker-entrypoint.sh postgres
